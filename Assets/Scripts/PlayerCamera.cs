67a93320d9c95a088b28d81f321ed8465227ecb8
Nicholas Terry <nicholasterry@familysearch.org>
2015-03-12 20:09:39 -0600

    Removed experimental assets and fixed player rotation

--- Assets/Scripts/PlayerCamera.cs
+++ Assets/Scripts/PlayerCamera.cs
@@ -1,28 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerCamera : MonoBehaviour {
-
-	public Transform target;
-	
+public class PlayerCamera : MonoBehaviour
+{
+	public Transform positionTarget;
+	public Transform lookTarget;
 	public float height = 2.0F;
 	public float damping = 2.0F;
 	public float distance = 2.0F;
 	
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
+		transform.position = positionTarget.position;
+		transform.LookAt (lookTarget);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-	
-	void LateUpdate() {
-		//return if we dont have a target
-		if (!target)
-			return;
 
-		transform.position = Vector3.Lerp (transform.position, new Vector3(target.position.x, target.position.y + height, target.position.z - distance), Time.deltaTime * damping);
+	void LateUpdate ()
+	{
+
 	}
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,31 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour {
-
-	public float speed = 6.0F;
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+	public float moveSpeed = 6.0F;
+	public float turnSpeed = 45.0F;
 	public float jumpSpeed = 8.0F;
 	public float gravity = 20.0F;
+
 	private Vector3 moveDirection = Vector3.zero;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
+	}
 
+	void FixedUpdate ()
+	{
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		CharacterController controller = GetComponent<CharacterController>();
-		if (controller.isGrounded) {
-			moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-			moveDirection = transform.TransformDirection(moveDirection);
-			moveDirection *= speed;
-			if (Input.GetButton("Jump"))
-				moveDirection.y = jumpSpeed;
-			
-		}
-		moveDirection.y -= gravity * Time.deltaTime;
-		controller.Move(moveDirection * Time.deltaTime);
+	void Update ()
+	{
+		CharacterController controller = GetComponent<CharacterController> ();
+
+		transform.Rotate (0.0F, Input.GetAxis ("Mouse X") * turnSpeed * Time.deltaTime, 0.0F);
+
+		moveDirection = new Vector3 (Input.GetAxis ("Horizontal"), 0.0F, Input.GetAxis ("Vertical"));
+		moveDirection.Normalize ();
+		moveDirection = transform.TransformDirection (moveDirection);
+
+		controller.Move (moveDirection * moveSpeed * Time.deltaTime);
 
 	}
 }
