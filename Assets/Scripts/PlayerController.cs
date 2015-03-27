e6842200d3cf90fe59ba4c885f1c1cdd140be21f
Nicholas Terry <nicholasterry@familysearch.org>
2015-03-26 20:05:38 -0600

    Removed remaining BlackSister assets and restored rudimentary gravity and jumping system

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,15 +7,18 @@ public class PlayerController : MonoBehaviour
 	public float moveSpeed = 6.0F;
 	public float turnSpeed = 45.0F;
 	public float jumpSpeed = 8.0F;
-	public float gravity = 20.0F;
-
+	public float gravity = 0.01F;
+	
+	
 	private Vector3 moveDirection = Vector3.zero;
-
+	private float health = 100.0F;
+	private float vSpeed = 0.0F;
+	
 	// Use this for initialization
 	void Start ()
 	{
 	}
-
+	
 	void FixedUpdate ()
 	{
 	}
@@ -24,14 +27,45 @@ public class PlayerController : MonoBehaviour
 	void Update ()
 	{
 		CharacterController controller = GetComponent<CharacterController> ();
-
+		
 		transform.Rotate (0.0F, Input.GetAxis ("Mouse X") * turnSpeed * Time.deltaTime, 0.0F);
-
+		
+		if (controller.isGrounded) {
+			if (Input.GetButton ("Jump")) {
+				vSpeed = 10.0F;
+			} else {
+				vSpeed = 0.0F;
+			}
+		} else {
+			vSpeed -= gravity * Time.deltaTime;
+		}
+		
+		Debug.Log (vSpeed);
+		
 		moveDirection = new Vector3 (Input.GetAxis ("Horizontal"), 0.0F, Input.GetAxis ("Vertical"));
 		moveDirection.Normalize ();
 		moveDirection = transform.TransformDirection (moveDirection);
-
+		
+		moveDirection.y = vSpeed * 0.15F;
+		
+		
+		
 		controller.Move (moveDirection * moveSpeed * Time.deltaTime);
-
+		
+		
+	}
+	
+	public void Damage(float damageAmount)
+	{
+		if (health > 0.0F) {
+			health -= damageAmount;
+		} else if (health <= 0.0F) {
+			health = 0.0F;
+			Die ();
+		}
+	}
+	
+	private void Die()
+	{
 	}
 }
