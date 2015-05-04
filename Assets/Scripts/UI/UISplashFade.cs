d17d74c6d1be8ff993706ad1adc18da44d8b781e
Nicholas Terry <nick.i.terry@gmail.com>
2015-05-03 20:07:23 -0600

    Added pgs logo splash image

---
+++ Assets/Scripts/UI/UISplashFade.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UISplashFade : MonoBehaviour {
+
+    public float fadeInDelay;
+    public float fadeOutDelay;
+    
+    CanvasRenderer rendererer;
+    Image image;
+
+	void Start () {
+        image = this.GetComponent<Image>();
+        rendererer = this.GetComponent<CanvasRenderer>();
+        rendererer.SetAlpha(0.0F);
+        DisplayImage(image, fadeInDelay, fadeOutDelay);
+	}
+	
+	void Update () {
+        
+	}
+
+    private void DisplayImage(Image img, float fadeInDelay, float fadeOutDelay)
+    {
+        StartCoroutine(DelayedFadeIn(img, fadeInDelay, fadeOutDelay));
+    }
+
+    private IEnumerator DelayedFadeIn(Image img, float fadeInDelay, float fadeOutDelay)
+    {
+        yield return new WaitForSeconds(fadeInDelay);
+        img.CrossFadeAlpha(1.0F, 1.0F, false);
+        StartCoroutine(DelayedFadeOut(img, fadeOutDelay));
+    }
+
+    private IEnumerator DelayedFadeOut(Image img, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        img.CrossFadeAlpha(0.0F, 1.0F, false);
+    }
+}
