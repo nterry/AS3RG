34ef96b835b0b37ae05cfed961654745e8bcd28e
Nicholas Terry <nick.i.terry@gmail.com>
2015-06-05 20:31:08 -0600

    Added Uni model and dogoo model and DOTween basic

---
+++ Assets/Scripts/DogooAnim.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+public class DogooAnim : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Sequence mySequence = DOTween.Sequence();
+        mySequence.Append(transform.DOMoveY(1.5f, 1));
+        mySequence.Join(transform.DOScaleY(0.15f, 1));
+        mySequence.SetLoops(-1, LoopType.Yoyo);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	   
+	}
+}
